79cfd07f42da74c09298c8278414fe5463310e2c
vysingsun <vysingsun@gmail.com>
2023-07-29 22:24:05 +0700

    config mongoDB

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/e_commerce', {
+    await mongoose.connect('mongodb+srv://vysingsun:1234@vysingsunapi.o08qbyp.mongodb.net/e-commerce?retryWrites=true&w=majority', {
       autoIndex: true,
       serverSelectionTimeoutMS: 30000 // default 30 seconds
     });
